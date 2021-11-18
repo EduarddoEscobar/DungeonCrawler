b390c480a8878e2d847a34b724cd0775c70850ce
Eduardo Escobar <eduarddo.escobar@gmail.com>
2021-11-17 19:50:21 -0500

    Made it so you could flip between different rooms

--- DungeonCrawler/Rooms/roomMaker.js
+++ DungeonCrawler/Rooms/roomMaker.js
@@ -1,15 +1,20 @@
 class Room{
   constructor(roomAttrs){
-    this.roomId = roomAttrs.roomId;
-    this.moveOptions = roomAttrs.moveOptions;
+    this.roomId = roomAttrs.roomId;//Number
+    this.moveOptions = roomAttrs.moveOptions;//Array of boolean values
+    this.enemies = roomAttrs.enemies;//Array of enemy objects
+    this.roomColor = roomAttrs.roomColor;// String of color
+    ///Boolean values indicating room status\\\
+    this.roomCleared = roomAttrs.roomCleared;
     this.hasTreasure = roomAttrs.hasTreasure;
-    this.enemies = roomAttrs.enemies;
     console.log(`New Room with id: ${this.roomId}:`);
   }
 
   draw(){
     let canvas = document.getElementById('myCanvas');
+    canvas.style.backgroundColor = this.roomColor;
     let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     this.enemies.forEach(enemy => {
       enemy.draw(canvas, Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height));
     });
--- DungeonCrawler/Rooms/test.js
+++ DungeonCrawler/Rooms/test.js
@@ -1,11 +1,18 @@
 let rooms = [];
 let count = 0;
+let roomTest = 0;
 const makeNewRoom = () => {
-  console.log(entities);
-  rooms.push(new Room({roomId: count++, moveOptions: [false, true, false, false], hasTreasure: false, enemies: [entities.Player, entities.Enemey]}));
+  rooms.push(new Room({roomId: count++, moveOptions: [false, true, false, false], hasTreasure: false, enemies: [entities.Player, entities.Enemey], roomColor: 'blue'}));
+  rooms.push(new Room({roomId: count++, moveOptions: [false, true, false, false], hasTreasure: false, enemies: [entities.Player, entities.Enemey], roomColor: 'red'}));
 
 }
 
+const flipBetweenRoom = () =>{
+  drawRoom(roomTest++);
+  if(roomTest === rooms.length){
+    roomTest = 0;
+  }
+}
 const drawRoom = (index) => {
   rooms[index].draw();
 }
